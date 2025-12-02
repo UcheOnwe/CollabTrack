f29adff40690efd509fd24cef27e42ccb01fe13f
Uchechukwu Onwe <maxoutguide24@gmail.com>
2025-12-02 11:12:17 -0600

    Added login page and updated server.js routes

--- Server.js
+++ Server.js
@@ -8,17 +8,20 @@ const PORT = 3000;
 app.set('view engine', 'ejs');
 
 // Allow Express to read form data (for POST /echo)
-app.use(express.urlencoded({ extended: false }));
+app.use(express.urlencoded({ extended: true }));
 
-// GET /  --> show home.ejs
 app.get('/', (req, res) => {
-  res.render('home');   // looks for Views/home.ejs
+  res.redirect('/login');
 });
 
-// POST /echo  --> handle the form submit from home.ejs
-app.post('/echo', (req, res) => {
-  const msg = req.body.msg || 'empty';
-  res.send(`You posted: <b>${msg}</b>`);
+app.get('/login', (req, res) => {
+    res.render('login');
+})
+
+app.post('/login', (req, res) => {
+    const {email, password } = req.body;
+    // Logic
+    res.send('Login attempt received: ${email}');
 });
 
 //  Start the server and keep it running
